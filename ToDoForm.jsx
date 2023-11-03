6fee614b85f115ecd935eeb7bac2d1de0a457487
fkammw <123600013+fkammw@users.noreply.github.com>
2023-11-03 11:04:42 -0600

    lab2 content added

---
+++ ToDoForm.jsx
@@ -0,0 +1,28 @@
+
+function ToDoForm(){
+
+    return(
+        <View style={styles.sectionContainer}>
+            <Text
+            style={[
+                styles.sectionTitle,
+                {
+                    color: isDarkMode ? Colors.white : Colors.black,
+                },
+            ]}>
+            {title}
+            </Text>
+            <Text
+            style={[
+                styles.sectionDescription,
+                {
+                    color: isDarkMode ? Colors.light : Colors.dark,
+                },
+            ]}>
+            {children}
+            </Text>
+        </View>
+    );
+}
+
+export default ToDoForm;
